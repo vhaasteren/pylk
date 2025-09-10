e0dda19c385123033fe9e04ed805e9b0fd6f5ae0
Rutger van Haasteren <vhaasteren@gmail.com>
2025-09-10 14:04:03 +0200

    Updated pyproject.toml and migrated away from setup.py and setup.cfg

--- pylk/__init__.py
+++ pylk/__init__.py
@@ -1,4 +1,8 @@
 # pylk/__init__.py
 __all__ = ["app", "main_window"]
-__version__ = "0.1.0"
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
 
